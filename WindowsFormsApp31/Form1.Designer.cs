463577676bbc5f12b99066efee20f58ec27c1156
zls <zls@DESKTOP-F3U79IB>
2020-01-10 16:51:45 +0800

    添加项目文件。

---
+++ WindowsFormsApp31/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace WindowsFormsApp31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(41, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 382);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApp31/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.IO;
+
+
+namespace WindowsFormsApp31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string str = "";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th1 = new Thread(test1);
+            th1.Start();
+
+            Thread th2 = new Thread(test2);
+            th2.Start();
+
+            timer1.Enabled = true;
+
+
+        }
+        public void test1()
+        {
+            try
+            {
+                if (!Directory.Exists(@"d:\test"))
+                    Directory.CreateDirectory(@"d:\test");
+                if (!File.Exists(@"d:\test\1.txt"))
+                    File.Create(@"d:\test\1.txt").Close();
+
+                StreamWriter sw = new StreamWriter(@"d:\test\1.txt", true);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    sw.WriteLine(i.ToString());
+
+                }
+                sw.Close();
+                int k = 0;
+                k = k / 0;//引发异常，写入事件log
+            }
+            catch(Exception ex)
+            {
+                StreamWriter sw = new StreamWriter(@"d:\test\1.txt", true);
+                sw.WriteLine(ex.ToString());
+                sw.Close();
+            }
+            
+        }
+        public void test2()
+        {
+            try
+            {
+                if (!Directory.Exists(@"d:\test"))
+                    Directory.CreateDirectory(@"d:\test");
+                if (!File.Exists(@"d:\test\2.txt"))
+                    File.Create(@"d:\test\2.txt").Close();
+
+                StreamWriter sw = new StreamWriter(@"d:\test\2.txt", true);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    sw.WriteLine(i.ToString());
+
+                }
+                sw.Close();
+            }
+            catch(Exception ex)
+            {
+                StreamWriter sw = new StreamWriter(@"d:\test\2.txt", true);
+                sw.WriteLine(ex.ToString());
+                sw.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            str += "1";
+            richTextBox1.Text = str;
+        }
+    }
+}
